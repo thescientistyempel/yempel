a1019d42c5c0250529fa68077434b90566035a0c
Catalina <145416282+thescientistyempel@users.noreply.github.com>
2023-11-15 08:33:27 -0500

    Create # Ping Pong Game in Code.py

---
+++ # Ping Pong Game in Code.py
@@ -0,0 +1,15 @@
+# Ping Pong Game in Code
+
+# Player 1 starts by printing "Hello, world!"
+print("Hello, world!")
+
+# Player 2 responds by assigning a value to x
+x = 93
+
+# Player 1 assigns a value to y based on x
+y = x + 7
+
+# Player 2 prints the value of y
+print(y)
+
+# And so on...
